708828f8aec99c61a3351c16597ea7e5b34315d6
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2025-08-13 15:01:28 +0200

    feat: Set up spring boot with jpa, h2, postgresql and spring-boot-starter-web

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 # Game Librarian
 ## References
 1. **Creating .gitignore file**  
-   Used [gitignore.io](https://www.toptal.com/developers/gitignore) to generate a `.gitignore` file for a Node.js project.
+   Used [gitignore.io](https://www.toptal.com/developers/gitignore) to generate a `.gitignore` file for a Node.js project in .gitignore
 2. **Creating Spring Boot structure**
    Used [Spring Initializr](https://start.spring.io/) to generate Spring Boot file structure.
+3. **Spring boot starters**
+   Used [Baeldung - Web Starter](https://www.baeldung.com/spring-boot-starters#bd-Starter) to implement web starter in application.yml and build.gradle.kts
+4. **Spring boot JPA**
+   Used [Baeldung - Data JPA Starter](https://www.baeldung.com/spring-boot-starters#bd-JPA) to implement JPA in application.yml and build.gradle.kts
--- gamelibrarian-backend/build.gradle.kts
+++ gamelibrarian-backend/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+	id("org.jetbrains.kotlin.plugin.jpa") version "1.9.25"
 	id("org.springframework.boot") version "3.5.4"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -19,7 +20,11 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("com.h2database:h2")
+	implementation("org.postgresql:postgresql:42.7.4")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
