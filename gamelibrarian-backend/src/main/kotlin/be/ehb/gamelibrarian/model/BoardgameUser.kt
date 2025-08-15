7f397d4ab693aeb0ca801999f31d4c679a0a6073
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2025-08-15 17:03:09 +0200

    feat: Creating BoardgameUser model
    Creating a model for boardgameuser with autoincremented user id's, a name and an email

---
+++ gamelibrarian-backend/src/main/kotlin/be/ehb/gamelibrarian/model/BoardgameUser.kt
@@ -0,0 +1,22 @@
+package be.ehb.gamelibrarian.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "Boardgame_users")
+data class BoardgameUser(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val boardgameUserId: Long = 0,
+
+    @Column(nullable = false)
+    val name: String,
+
+    @Column(nullable = false, unique = true)
+    val email: String
+)
