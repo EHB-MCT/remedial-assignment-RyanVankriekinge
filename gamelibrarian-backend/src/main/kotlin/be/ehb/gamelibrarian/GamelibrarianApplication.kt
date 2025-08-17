b83a6c1dd97fdcada4d0c725a27a773402bcf79c
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2025-08-17 22:24:33 +0200

    chore: adding server address for live front-end server to CORS in configuration file

--- gamelibrarian-backend/src/main/kotlin/be/ehb/gamelibrarian/GamelibrarianApplication.kt
+++ gamelibrarian-backend/src/main/kotlin/be/ehb/gamelibrarian/GamelibrarianApplication.kt
@@ -2,9 +2,22 @@ package be.ehb.gamelibrarian
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class GamelibrarianApplication
+class GamelibrarianApplication {
+	@Bean
+	fun corsConfigurer(): WebMvcConfigurer {
+		return object : WebMvcConfigurer {
+			override fun addCorsMappings(registry: CorsRegistry) {
+				registry.addMapping("/api/**").allowedOrigins("http://127.0.0.1:5500")
+					.allowedMethods("GET", "POST", "PUT", "DELETE")
+			}
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<GamelibrarianApplication>(*args)
