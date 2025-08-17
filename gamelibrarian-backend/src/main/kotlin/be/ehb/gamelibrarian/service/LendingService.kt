7a6ffe50e4354b24becb6e315b3c2ced185cfc9b
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2025-08-17 18:05:03 +0200

    feat: Incrementing popularity of a boardgame when new lending instance is created

--- gamelibrarian-backend/src/main/kotlin/be/ehb/gamelibrarian/service/LendingService.kt
+++ gamelibrarian-backend/src/main/kotlin/be/ehb/gamelibrarian/service/LendingService.kt
@@ -1,6 +1,7 @@
 package be.ehb.gamelibrarian.service
 
 import be.ehb.gamelibrarian.model.Lending
+import be.ehb.gamelibrarian.repository.BoardgameRepository
 import be.ehb.gamelibrarian.repository.LendingRepository
 import be.ehb.gamelibrarian.repository.LoanStatusRepository
 import org.springframework.stereotype.Service
@@ -10,16 +11,28 @@ import java.time.LocalDateTime
 @Service
 class LendingService(
     private val lendings: LendingRepository,
-    private val statusRepository: LoanStatusRepository
+    private val statusRepository: LoanStatusRepository,
+    private val boardgameRepository: BoardgameRepository
 ) {
     @Transactional
-    fun create(lending: Lending): Lending =
-        lendings.save(lending)
+    fun create(lending: Lending): Lending {
+        val savedLending = lendings.save(lending)
+        val boardgame = savedLending.copy.boardgame
+
+        val updatedBoardgame = boardgame.copy(
+            popularity = boardgame.popularity + 1
+        )
+
+        boardgameRepository.save(updatedBoardgame)
+
+        return savedLending
+    }
 
     fun get(id: Long): Lending =
         lendings.findById(id).orElseThrow { NoSuchElementException("Lending $id not found") }
 
     fun getAll(): List<Lending> = lendings.findAll()
+    @Transactional
     fun markAsReturned(loanId: Long): Lending {
         val lending = lendings.findById(loanId)
             .orElseThrow { NoSuchElementException("Lending $loanId not found") }
@@ -31,6 +44,6 @@ class LendingService(
             returnedAt = LocalDateTime.now(),
             status = returnedStatus
         )
-        return updatedLending
+        return lendings.save(updatedLending)
     }
 }
